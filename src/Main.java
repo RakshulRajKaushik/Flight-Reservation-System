d0da9e044637f49e3b2607b7d477f1a286bca298
RakshulRajKaushik <59829180+RakshulRajKaushik@users.noreply.github.com>
2021-01-19 23:23:31 +0530

    checkpoint 5 completed

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,20 @@ public class Main {
 	/**
 	 * @param args
 	 */
+	public static void printTicketDetails(Ticket ticket) {
+			System.out.println(ticket.getPnr());
+		}
 	public static void main(String[] args) {
+		
+		TouristTicket t=new TouristTicket("fhg");
+		RegularTicket r=new RegularTicket("spa");
+		printTicketDetails(t);
+		printTicketDetails(r);
+		
+		
+		
 		// TODO Auto-generated method stub
-		Flight f=new Flight("2153", "zet", 100, 25);
+//		Flight f=new Flight("2153", "zet", 100, 25);
 //		Passenger p=new Passenger(53);
 //		Ticket t=new Ticket("5456", "delhi", "goa",FlightObject, "10Am", "10pm",PassengerObject, "5465f",500.50f,false);
 	}
